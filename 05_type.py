10370f7ba7795bda59d459d55632c435d6f5b9e5
neev151 <mehtaneev991@gmail.com>
2024-10-01 03:45:30 -0700

    modified

--- 05_type.py
+++ 05_type.py
@@ -1,12 +1,16 @@
 a = 10
 t = type(a) # class <int>
+print(t)
 
-b = 29.8
-n = type(b) # class <flo>
+# b = 29.8
+# n = type(b) # class <flo>
+# print(n)
 
-c = "neev"
-m = type(c) # class <str>
+# c = "neev"
+# m = type(c) # class <str>
+# print(m)
 
-d = "True"
-o = type(d) # class <boolean>
+# d = "True"
+# o = type(d) # class <boolean>
+# print(o)
 
