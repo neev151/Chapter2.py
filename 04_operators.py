726c146d341dcb2bd1c4e6e71cce1ddc4c28e195
neev151 <mehtaneev991@gmail.com>
2024-10-01 03:28:58 -0700

    new message is added

--- 04_operators.py
+++ 04_operators.py
@@ -28,4 +28,21 @@ d=10>6
 
 print(d)
 
+#logical operators
 
+e = True or False
+
+print (e)
+
+
+# Truth table of 'is'
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth table of 'and'
+print("True and False is", True and False)
+print("True and True is", True and True)
+print("False and True is", False and True)
+print("False and False is", False and False)
