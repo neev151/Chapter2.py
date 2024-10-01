5210da97879d2e6ee2b7418ab95d26c5362fe70e
neev151 <mehtaneev991@gmail.com>
2024-10-01 03:12:35 -0700

    new message is added

--- 04_operators.py
+++ 04_operators.py
@@ -10,13 +10,22 @@ print(a + b)
 
 # assigment operators
 
-a = 32
+a = 32-12 # assign 32-12 in a
+
 print (a)
 
-b = 76
+b = 760
 
-b += 92
+# b += 92 # increment the value of b by 92 and assign in b
+
+b -= 501 # Decrement the value of b by 92 and assign in b
 
 print(b)
+
+#comparison operators
+
+d=10>6
+
+print(d)
 
 
