48f3a8a1065cc8237c1f02e9793e19221da933f7
neev151 <mehtaneev991@gmail.com>
2024-10-01 03:34:55 -0700

    hello

--- 04_operators.py
+++ 04_operators.py
@@ -46,3 +46,5 @@ print("True and False is", True and False)
 print("True and True is", True and True)
 print("False and True is", False and True)
 print("False and False is", False and False)
+
+print(not(True))
