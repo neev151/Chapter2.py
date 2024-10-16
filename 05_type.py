b2d00f7b37a35881f7ed14cbbd5cdd7216fee3a2
neev151 <dhuladev27@gmail.com>
2024-10-16 03:54:25 -0700

    new message is added

--- 05_type.py
+++ 05_type.py
@@ -1,14 +1,14 @@
-# a = 10
-# t = type(a) # class <int>
-# print(t)
+a = 10
+t = type(a) # class <int>
+print(t)
 
-# b = 29.8
-# n = type(b) # class <flo>
-# print(n)
+b = 29.8
+n = type(b) # class <flo>
+print(n)
 
-# c = "neev"
-# m = type(c) # class <str>
-# print(m)
+c = "neev"
+m = type(c) # class <str>
+print(m)
 
 d = "False"
 o = type(d) # class <boolean>
