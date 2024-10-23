87d1cc24240b8ef7d2e661de4bd52ecc8d06c003
neev151 <dhuladev27@gmail.com>
2024-10-23 03:15:56 -0700

    nw modification is added

--- 06_input.py
+++ 06_input.py
@@ -1,9 +1,11 @@
-a = input("enter number 1")
+a = input("Enter number 1: ")
 
-b = input("enter number 2")
+b = input("Enter number 2: ")
 
 print("Number a is: ",a)
-
 print("Number b is: ",b)
+print("Sum is ", a + b)
+
+
 
 
